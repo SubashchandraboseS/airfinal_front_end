986667e5c95688b86a191284e90646fb118cf180
Subashchandrabose <subash@localhost.localdomain>
2024-02-05 21:42:26 +0530

    many routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+// App.js
 import React from 'react';
-import ContactForm from './components/ContactForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage/Homepage';
+import LoginPage from './components/LoginPage/LoginPage';
+import TicketBooking from './components/TicketBooking/TicketBooking'; // Import your TicketBooking component
+import AddFlight from './components/AddFlight/AddFlight'; // Adjust the path based on your project structure
+
+import { AuthProvider } from './components/AuthContext/AuthContext';
 
 function App() {
   return (
-    <div>
-      <h1>Contact Form</h1>
-      <ContactForm />
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/ticket-booking" element={<TicketBooking />} />
+          <Route path="/addflight" element={<AddFlight />} />
+        
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
